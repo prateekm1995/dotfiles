c9fe0db305232d7d99bb629dd827da1bc650ea71
Prateek Malhotra <prateek@Prateeks-MacBook-Air.local>
2026-07-03 09:47:01 +0530

    added tpm

--- install.sh
+++ install.sh
@@ -8,4 +8,16 @@ echo "Creating symlinks..."
 
 ln -sfn "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
 
+echo "Installing TPM..."
+
+mkdir -p "$HOME/.tmux/plugins"
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+
+    git clone https://github.com/tmux-plugins/tpm \
+
+        "$HOME/.tmux/plugins/tpm"
+
+fi
+
 echo "Done!"
